da4d4c82c65de52d9708c89daaeee5c499b8d86d
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-01-04 01:52:18 +0500

    exphbs template engine added

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const dbConnection = require('./config/db')
+const exphbs = require('express-handlebars')
 
 
 //Load config 
 dotenv.config({path:'./config/config.env'})
 
 dbConnection()
+
+//logging
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+//use template engine
+app.engine('.hbs', exphbs({defaultLayout:'main' ,extname:'.hbs'}))
+app.set('view engine', '.hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
 
 app.listen(process.env.PORT || 5000,console.log(`server ${process.env.PORT} ga ulandi`))
